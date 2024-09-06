8364d2239ba4163d45fa5ea41c6622dc35eb3b1f
Zikai Chen <natnstart@gmail.com>
2024-09-07 00:03:52 +0900

    Modify Notes component to read directly from the mongodb database.

--- src/components/notes.tsx
+++ src/components/notes.tsx
@@ -1,8 +1,29 @@
-import {notes} from "../dto/note";
+import React from "react";
+
+import {Note} from "../dto/note";
 import NoteItem from "./noteItem";
 
 const Notes = () => {
+    const [notes, setNotes] = React.useState<Note[]>([]);
     const reversedNotes = [...notes].reverse();
+
+    React.useEffect(() => {
+       async function fetchNotes() {
+           try {
+               const response = await fetch(`${process.env.REACT_APP_API}/note`);
+               if (!response.ok) {
+                   console.log("Failed to fetch notes");
+               }
+               const notes = await response.json() as Note[];
+               setNotes(notes);
+           } catch (error) {
+               console.error(error);
+           }
+       }
+
+       void fetchNotes();
+    });
+
     return (
         <main className="center">
             <h1>Notes</h1>
--- src/dto/note.ts
+++ src/dto/note.ts
@@ -8,28 +8,3 @@ type Note = {
 }
 
 export type {Note};
-export const notes: Note[] = [
-    /* {
-        id: 1,
-        author: "acst",
-        title: "Test Note",
-        categories: ["test"],
-        keywords: ["test"]
-    }, */
-    {
-        id: 2,
-        author: "acst",
-        title: "蛇和包子过新年",
-        categories: ["test"],
-        keywords: ["test"],
-        content: "蛇和包子过新年"
-    },
-    {
-        id: 3,
-        author: "acst",
-        title: "Access Your Device Anywhere with SSH Reverse Tunneling",
-        categories: ["CS", "Networking"],
-        keywords: ["Reverse Tunneling", "SSH", "Networking", "AWS"],
-        content: "Access Your Device Anywhere with SSH Reverse Tunneling"
-    }
-];
