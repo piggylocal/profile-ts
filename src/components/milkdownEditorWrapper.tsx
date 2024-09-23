2204e80d85b8eac9c835759235f859b16bf4d0ac
K <cktwooooo@gmail.com>
2024-09-23 23:50:30 +0900

    Dev: Auto focus on MilkdownEditor.

--- src/components/milkdownEditorWrapper.tsx
+++ src/components/milkdownEditorWrapper.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Editor, rootCtx} from '@milkdown/kit/core';
 import {nord} from '@milkdown/theme-nord';
-import {Milkdown, MilkdownProvider, useEditor} from '@milkdown/react';
+import {Milkdown, MilkdownProvider, useEditor, useInstance} from '@milkdown/react';
 import {commonmark} from '@milkdown/kit/preset/commonmark';
+import {editorViewCtx} from '@milkdown/core';
 
 const MilkdownEditor: React.FC = () => {
-    useEditor((root) =>
+   useEditor((root) =>
         Editor.make()
             .config(nord)
             .config((ctx) => {
@@ -13,6 +14,16 @@ const MilkdownEditor: React.FC = () => {
             })
             .use(commonmark),
     );
+
+    const [, getInstance] = useInstance();
+    const instance = getInstance();
+
+    useEffect(() => {
+        instance?.action((ctx) => {
+            const view = ctx.get(editorViewCtx);
+            view.focus();
+        });
+    }, [instance]);
     return <Milkdown/>;
 };
 
