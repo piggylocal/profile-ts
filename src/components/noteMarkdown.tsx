c019e5c089e17a21594b7165d203ffda25b70a49
K <cktwooooo@gmail.com>
2024-09-24 23:06:07 +0900

    Dev: use codemirror as markdown editor

--- src/components/noteMarkdown.tsx
+++ src/components/noteMarkdown.tsx
@@ -1,32 +1,36 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
 import {useLocalStorage} from "@uidotdev/usehooks";
+import CodeMirror from '@uiw/react-codemirror';
+import {markdown, markdownLanguage} from '@codemirror/lang-markdown';
+import {languages} from '@codemirror/language-data';
 
-const NoteMarkdown = ({activePanelValue}: {activePanelValue: string}) => {
-    const ref = React.useRef<HTMLTextAreaElement>(null);
+const NoteMarkdown = ({activePanelValue}: { activePanelValue: string }) => {
     const [content, setContent] = useLocalStorage("content", "");
+    const [code, setCode] = React.useState<string>("");
 
     React.useEffect(() => {
-        let element = ref.current;
-        if (element === null) {
-            return;
-        }
-        element.value = content;
-    }, [content]);
+        setCode(content);
+    }, [content, setCode]);
     React.useEffect(() => {
         if (activePanelValue === "markdown") {
             return;
         }
-        setContent(ref.current?.value ?? "");
-    }, [activePanelValue, setContent])
+        console.log(code);
+        setContent(code);
+    }, [activePanelValue, code, setContent])
 
     return (
-        <TextField
+        /*<TextField
             inputRef={ref}
             label="Markdown"
             multiline
             rows={20}
             fullWidth={true}
+        />*/
+        <CodeMirror
+            value={code}
+            onChange={(value) => setCode(value)}
+            extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]}
         />
     )
 };
