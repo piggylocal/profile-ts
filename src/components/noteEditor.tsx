b8f2e5fea46c87e2c29e037b303911ce9ee02344
Zikai Chen <natnstart@gmail.com>
2024-09-29 21:29:36 +0900

    Feat: Create a snackbar on saving draft.

--- src/components/noteEditor.tsx
+++ src/components/noteEditor.tsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import {Button, Stack} from "@mui/material";
 import {useNavigate, useLocation} from "react-router-dom";
 import {useLocalStorage} from "@uidotdev/usehooks";
+import Snackbar, {SnackbarCloseReason} from '@mui/material/Snackbar';
 
 import "../styles/editor.css";
 import {MilkdownEditorWrapper} from "./milkdownEditorWrapper";
@@ -22,7 +23,7 @@ const TabPanel = ({children, value, editorType}: {
             hidden={value !== editorType}
             aria-labelledby={`editor-tab-${editorType}`}
         >
-            <Box sx={{ paddingTop: 3, paddingBottom: 3 }}>
+            <Box sx={{paddingTop: 3, paddingBottom: 3}}>
                 {children}
             </Box>
         </div>
@@ -40,6 +41,7 @@ const NoteEditor = () => {
     // state transitions before we actually navigate to the target. After we navigate
     // to the target, NO state transition is allowed.
     const [target, setTarget] = React.useState<EditorTarget>(EditorTarget.INIT);
+    const [saveDraftBarOpen, setSaveDraftBarOpen] = React.useState<boolean>(false);
 
     const navigate = useNavigate();
 
@@ -53,10 +55,22 @@ const NoteEditor = () => {
     // is updated.
     const storeContent = () => {
         setContentStore(content);
+        if (target === EditorTarget.DRAFT) {
+            setSaveDraftBarOpen(true);
+        }
         setTarget(value);
         if (target === EditorTarget.BACK) {
             navigate(`${location.pathname}/..`);
         }
+    }
+
+    const handleSaveDraftBarClose = (
+        _: React.SyntheticEvent | Event,
+        reason?: SnackbarCloseReason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setSaveDraftBarOpen(false);
     }
 
     React.useEffect(() => {
@@ -64,61 +78,69 @@ const NoteEditor = () => {
     }, [location]);
 
     return (
-        <main className="editor center">
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={value} onChange={handleChange} aria-label="editor tabs">
-                    <Tab label="WYSIWYG" value={EditorTarget.WYSIWYG} />
-                    <Tab label="Markdown" value={EditorTarget.MARKDOWN} />
-                </Tabs>
-            </Box>
-            <TabPanel editorType={EditorTarget.WYSIWYG} value={value}>
-                <MilkdownEditorWrapper
-                    target={target}
-                    setTarget={setTarget}
-                    content={content}
-                    setContent={setContent}
-                    editorValue={value}
-                    setEditorValue={setValue}
-                    storeContent={storeContent}
-                />
-            </TabPanel>
-            <TabPanel editorType={EditorTarget.MARKDOWN} value={value}>
-                <NoteMarkdown
-                    target={target}
-                    setTarget={setTarget}
-                    content={content}
-                    setContent={setContent}
-                    editorValue={value}
-                    setEditorValue={setValue}
-                    storeContent={storeContent}
-                />
-            </TabPanel>
-            <Stack
-                className="button-panel"
-                direction="row"
-                spacing={2}
-                sx={{
-                    justifyContent: "flex-end",
-                    alignItems: "center",
-                }}
-            >
-                <Button
-                    variant="outlined"
-                    style={{marginRight: "auto"}}
-                    onClick={() => setTarget(EditorTarget.BACK)}
+        <>
+            <main className="editor center">
+                <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                    <Tabs value={value} onChange={handleChange} aria-label="editor tabs">
+                        <Tab label="WYSIWYG" value={EditorTarget.WYSIWYG}/>
+                        <Tab label="Markdown" value={EditorTarget.MARKDOWN}/>
+                    </Tabs>
+                </Box>
+                <TabPanel editorType={EditorTarget.WYSIWYG} value={value}>
+                    <MilkdownEditorWrapper
+                        target={target}
+                        setTarget={setTarget}
+                        content={content}
+                        setContent={setContent}
+                        editorValue={value}
+                        setEditorValue={setValue}
+                        storeContent={storeContent}
+                    />
+                </TabPanel>
+                <TabPanel editorType={EditorTarget.MARKDOWN} value={value}>
+                    <NoteMarkdown
+                        target={target}
+                        setTarget={setTarget}
+                        content={content}
+                        setContent={setContent}
+                        editorValue={value}
+                        setEditorValue={setValue}
+                        storeContent={storeContent}
+                    />
+                </TabPanel>
+                <Stack
+                    className="button-panel"
+                    direction="row"
+                    spacing={2}
+                    sx={{
+                        justifyContent: "flex-end",
+                        alignItems: "center",
+                    }}
                 >
-                    Back
-                </Button>
-                <Button
-                    variant="outlined"
-                    color="secondary"
-                    onClick={() => setTarget(EditorTarget.DRAFT)}
-                >
-                    Save draft
-                </Button>
-                <Button variant="outlined">Next</Button>
-            </Stack>
-        </main>
+                    <Button
+                        variant="outlined"
+                        style={{marginRight: "auto"}}
+                        onClick={() => setTarget(EditorTarget.BACK)}
+                    >
+                        Back
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="secondary"
+                        onClick={() => setTarget(EditorTarget.DRAFT)}
+                    >
+                        Save draft
+                    </Button>
+                    <Button variant="outlined">Next</Button>
+                </Stack>
+            </main>
+            <Snackbar
+                open={saveDraftBarOpen}
+                autoHideDuration={3000}
+                onClose={handleSaveDraftBarClose}
+                message="Draft saved."
+            />
+        </>
     )
 }
 
