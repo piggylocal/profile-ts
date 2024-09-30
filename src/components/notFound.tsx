ff42794854f7a5aeae96bc294c1495aa86ed415c
Zikai Chen <natnstart@gmail.com>
2024-10-01 00:31:14 +0900

    Fix: Make 404 page more funny 🐷.

--- src/components/notFound.tsx
+++ src/components/notFound.tsx
@@ -4,7 +4,7 @@ const NoteFound = () => {
             <div className="about">
                 <div className="person_info">
                     <img className="person" src="https://i.imgur.com/JGEx3J8.png" alt=""/>
-                    <p>Nobody is here...</p>
+                    <p>Nobody's here...</p>
                 </div>
             </div>
         </main>
