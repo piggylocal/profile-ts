99355263816e808b9dc600fd3342b57b032a6c54
Zikai Chen <natnstart@gmail.com>
2024-11-03 03:26:59 +0900

    Feat: Finish imgur image auto-upload!!

--- src/components/milkdownEditorWrapper.tsx
+++ src/components/milkdownEditorWrapper.tsx
@@ -2,12 +2,65 @@ import React, {useEffect} from 'react';
 import {Editor, rootCtx} from '@milkdown/kit/core';
 import {nord} from '@milkdown/theme-nord';
 import {Milkdown, MilkdownProvider, useEditor, useInstance} from '@milkdown/react';
-import {upload} from '@milkdown/plugin-upload';
 import {commonmark} from '@milkdown/kit/preset/commonmark';
 import {editorViewCtx} from '@milkdown/core';
 import {getMarkdown, replaceAll} from '@milkdown/kit/utils';
+import {upload, uploadConfig, Uploader} from '@milkdown/kit/plugin/upload';
+import type {Node} from '@milkdown/kit/prose/model';
+import axios from "axios";
 
 import {EditorTarget} from "../configs/note";
+import {getImgurToken} from "../managers/imgurOAuth";
+
+const uploader: Uploader = async (files, schema) => {
+    const imgurToken = getImgurToken();
+    const images: File[] = [];
+
+    for (let i = 0; i < files.length; i++) {
+        const file = files.item(i);
+        if (!file || !file.type.includes('image')) {
+            continue;
+        }
+        images.push(file);
+    }
+
+    const nodes: Node[] = await Promise.all(
+        images.map(async (image) => {
+            if (!imgurToken) {
+                console.error('No imgur token found');
+                return schema.text('No imgur token found');
+            }
+
+            const formData = new FormData();
+            formData.append('image', image);
+            formData.append('type', 'file');
+            formData.append('title', image.name);
+            let src: string;
+            try {
+                const response = await axios.post(
+                    'https://api.imgur.com/3/image',
+                    formData,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${imgurToken}`,
+                        },
+                    },
+                );
+                src = response.data.data.link;
+            } catch (error) {
+                console.error(error);
+                return schema.text('Failed to upload image');
+            }
+            const alt = image.name;
+            return schema.nodes.image.createAndFill({
+                src,
+                alt,
+            }) as Node;
+        }),
+    );
+
+    return nodes;
+};
 
 const MilkdownEditor = ({target, setTarget, content, setContent, editorValue, setEditorValue, storeContent}: {
     target: EditorTarget,
@@ -23,6 +76,12 @@ const MilkdownEditor = ({target, setTarget, content, setContent, editorValue, se
             .config(nord)
             .config((ctx) => {
                 ctx.set(rootCtx, root);
+            })
+            .config((ctx) => {
+                ctx.update(uploadConfig.key, (prev) => ({
+                    ...prev,
+                    uploader,
+                }));
             })
             .use(commonmark)
             .use(upload),
